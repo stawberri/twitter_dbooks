c2ba8bc6fc6fe214a41f76fe3e49ff3ea232e309
Stawberri <aideen@aideen.pw>
2015-01-04 00:07:34 -0800

    Made all configuration go into @config

--- bots.rb
+++ bots.rb
@@ -3,26 +3,41 @@ require 'ostruct'
 
 # Main twitterbot class
 class DbooksBot < Ebooks::Bot
-  # Config variable. Doesn't need to be an accessor.
+  # Config openstruct
   attr_reader :config
+  # Default danbooru request parameters
+  attr_reader :danbooru_default_params
 
   # Inital twitterbot setup
   def configure
-    # Load twitter configuration from environment variables.
-    @consumer_key = ENV['TWITTER_KEY']
-    @consumer_secret = ENV['TWITTER_SECRET']
-    @access_token = ENV['TWITTER_TOKEN']
-    @access_token_secret = ENV['TWITTER_TSECRET']
-
-    # Load other configuration options
+    # Load configuration from environment variables
     @config = OpenStruct.new
-    @config.danbooru_login = ENV['DANBOORU_LOGIN']
-    @config.danbooru_key = ENV['DANBOORU_KEY']
-    @config.danbooru_tags = ENV['DANBOORU_TAGS']
-    @config.tweet_interval = ENV['TWEET_INTERVAL']
+    @config.twitter_key = ENV['TWITTER_KEY'].chomp
+    @config.twitter_secret = ENV['TWITTER_SECRET'].chomp
+    @config.twitter_token = ENV['TWITTER_TOKEN'].chomp
+    @config.twitter_tsecret = ENV['TWITTER_TSECRET'].chomp
+    @config.danbooru_login = ENV['DANBOORU_LOGIN'].chomp
+    @config.danbooru_key = ENV['DANBOORU_KEY'].chomp
+    @config.danbooru_tags = ENV['DANBOORU_TAGS'].chomp
+    @config.tweet_interval = ENV['TWEET_INTERVAL'].chomp
+
+    # Load configuration into twitter variables
+    @consumer_key = config.twitter_key
+    @consumer_secret = config.twitter_secret
+    @access_token = config.twitter_token
+    @access_token_secret = config.twitter_tsecret
+
+    # Setup default danbooru params with danbooru login info
+    @danbooru_default_params = {}
+    unless config.danbooru_login.empty? && config.danbooru_key.empty?
+      @danbooru_default_params['login'] = URI.escape config.danbooru_login
+      @danbooru_default_params['api_key'] = URI.escape config.danbooru_key
+    end
   end
 
+  # When twitter bot starts up
   def on_startup
+    # Repeat this every tweet_interval
     scheduler.every config.tweet_interval do
     end
   end
