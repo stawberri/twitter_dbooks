6703af4c077d1d49b24a5cdb1082c573515c37dd
Stawberri <aideen@aideen.pw>
2015-01-04 02:51:04 -0800

    Post tweeting method now tweets posts
    Fixed a bug in tweetpic.rb

--- bots.rb
+++ bots.rb
@@ -5,42 +5,34 @@ require 'json'
 
 require_relative 'tweetpic'
 
-# Main twitterbot class
-class DbooksBot < Ebooks::Bot
-  # Config openstruct
-  attr_reader :config
+# Module used to extend string with helper functions
+module PuddiString
+  # Trim a string, optionally adding a thing at the end.
+  def trim(len)
+    # Just call the other function.
+    trim_cap(len, '')
+  end
+  def trim_ellipsis(len)
+    # Special one for this hard to find character
+    trim_cap(len, '…')
+  end
+  def trim_cap(len, cap)
+    # Make sure inputs are the right type.
+    len = len.to_i
+    cap = cap.to_s
+    # First, check if the string is already within the length.
+    return self if length <= len
+    # It's not, so find out how short we have to trim to.
+    real_length = len - cap.length
+    # Now just return the trimmed string!
+    self[0...real_length] + cap
+  end
+end
+
+# Module used to extend Ebooks with Danbooru features
+module Danbooru
   # Default danbooru request parameters
   attr_reader :danbooru_default_params
-
-  # Inital twitterbot setup
-  def configure
-    # Load configuration from environment variables
-    @config = OpenStruct.new
-    @config.twitter_key = ENV['TWITTER_KEY'].chomp
-    @config.twitter_secret = ENV['TWITTER_SECRET'].chomp
-    @config.twitter_token = ENV['TWITTER_TOKEN'].chomp
-    @config.twitter_tsecret = ENV['TWITTER_TSECRET'].chomp
-    @config.danbooru_login = ENV['DANBOORU_LOGIN'].chomp
-    @config.danbooru_key = ENV['DANBOORU_KEY'].chomp
-    @config.danbooru_tags = ENV['DANBOORU_TAGS'].chomp
-    @config.tweet_interval = ENV['TWEET_INTERVAL'].chomp
-
-    # Load configuration into twitter variables
-    @consumer_key = config.twitter_key
-    @consumer_secret = config.twitter_secret
-    @access_token = config.twitter_token
-    @access_token_secret = config.twitter_tsecret
-
-    # Grab username if all of those variables have been set already
-    @username = twitter.user.screen_name if @access_token && @access_token_secret && @consumer_key && @consumer_secret
-
-    # Setup default danbooru params with danbooru login info
-    @danbooru_default_params = {}
-    unless config.danbooru_login.empty? && config.danbooru_key.empty?
-      @danbooru_default_params['login'] = config.danbooru_login
-      @danbooru_default_params['api_key'] = config.danbooru_key
-    end
-  end
 
   # Wrapper for danbooru requests
   def danbooru_get(query = 'posts', parameters = {})
@@ -80,17 +72,136 @@ class DbooksBot < Ebooks::Bot
     danbooru_get 'posts', page: page, limit: 100, tags: tags
   end
 
-  # Tweet a post with its post data
-  def danbooru_tweet_post(post_data)
-    # Make post_data a hash if it isn't one already
-    post_data = post_data[0] if post_data.is_a? Array
-    post_data = danbooru_get("posts/#{post_data}") unless post_data.is_a? Hash
+  # Genreates a little string to tweet with links.
+  def danbooru_tag_string(post)
+    # Convert a string with just spaces into a friendly comma separated form with ands.
+    def get_tag_string(spaced_string, remove_quotes = false)
+      working_array = spaced_string.split ' '
+      if remove_quotes
+        working_array.map! do |string|
+          str = string.gsub(/_\(.*\z/, '')
+          str.gsub(/_/, ' ')
+        end
+      else
+        working_array.map! do |string|
+          string.gsub(/_/, ' ')
+        end
+      end
 
-    bot.log "Preparing to tweet post \##{post['id']}"
+      case working_array.length
+      when 0
+        nil
+      when 1
+        working_array[0]
+      when 2
+        working_array.join ' and '
+      else
+        working_array[-1] = 'and ' + working_array[-1]
+        working_array.join ', '
+      end
+    end
+
+    # Generate tag strings
+    tags_character = get_tag_string post.tag_string_character, true
+    tags_copyright = get_tag_string post.tag_string_copyright
+    tags_artist = get_tag_string post.tag_string_artist
+
+    output = ''
+    if tags_character.nil?
+      if tags_copyright.nil?
+        # No character or copyright
+        output = 'drawn'
+      else
+        # No character, but has copyright
+        output = tags_copyright
+      end
+    else
+      # Has characters
+      output = tags_character
+      unless tags_copyright.nil?
+        output += " (#{tags_copyright})"
+      end
+    end
+
+    output += ' by ' + tags_artist unless tags_artist.nil?
+
+    output
+  end
+
+  # Tweet a post with its post data
+  def danbooru_tweet_post(post)
+    # Make post an OpenStruct
+    post = post[0] if post.is_a? Array
+    post = danbooru_get("posts/#{post}") unless post.is_a? Hash
+    post = OpenStruct.new post
+
+    # Is post in a format we like?
+    return false unless ['jpg','jpeg','png'].include? post.file_ext
+
+    # Is it sensitive?
+    sensitive = post.rating != 's'
+
+    # Get a tag string
+    tag_string = danbooru_tag_string post
+    tag_string.extend PuddiString
+    tag_string = ' ' + tag_string.trim_ellipsis(94) unless tag_string.empty?
+
+    # Get post URI
+    post_uri = "https://danbooru.donmai.us/posts/#{post.id}"
+
+    # Get image URI
+    image_uri = "https://danbooru.donmai.us/#{post.large_file_url}"
+
+    # Tweet post!
+    log "Tweeting post #{post.id}, rating: #{post.rating}"
+    pic_tweet("#{post_uri}#{tag_string}", image_uri, possibly_sensitive: sensitive)
+
+    true
+  end
+end
+
+# Main twitterbot class
+class DbooksBot < Ebooks::Bot
+  include Danbooru
+
+  # Config openstruct
+  attr_reader :config
+
+  # Inital twitterbot setup
+  def configure
+    # Load configuration from environment variables
+    @config = OpenStruct.new
+    @config.twitter_key = ENV['TWITTER_KEY'].chomp
+    @config.twitter_secret = ENV['TWITTER_SECRET'].chomp
+    @config.twitter_token = ENV['TWITTER_TOKEN'].chomp
+    @config.twitter_tsecret = ENV['TWITTER_TSECRET'].chomp
+    @config.danbooru_login = ENV['DANBOORU_LOGIN'].chomp
+    @config.danbooru_key = ENV['DANBOORU_KEY'].chomp
+    @config.danbooru_tags = ENV['DANBOORU_TAGS'].chomp
+    @config.tweet_interval = ENV['TWEET_INTERVAL'].chomp
+
+    # Load configuration into twitter variables
+    @consumer_key = config.twitter_key
+    @consumer_secret = config.twitter_secret
+    @access_token = config.twitter_token
+    @access_token_secret = config.twitter_tsecret
+
+    # Grab username if all of those variables have been set already
+    @username = twitter.user.screen_name if @access_token && @access_token_secret && @consumer_key && @consumer_secret
+
+    # Setup default danbooru params with danbooru login info
+    @danbooru_default_params = {}
+    unless config.danbooru_login.empty? && config.danbooru_key.empty?
+      @danbooru_default_params['login'] = config.danbooru_login
+      @danbooru_default_params['api_key'] = config.danbooru_key
+    end
   end
 
   # When twitter bot starts up
   def on_startup
+    danbooru_tweet_post danbooru_posts
+
+    exit;
 
     # Repeat this every tweet_interval
     scheduler.every config.tweet_interval do
