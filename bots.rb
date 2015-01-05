9b7d921b591096c26a03848f12624936fd022122
Stawberri <aideen@aideen.pw>
2015-01-05 01:00:57 -0800

    Revert "Use port environment variable"
    This reverts commit a7e8ab11111d2377b21e42a3f5d523bbfa01d5f0.

--- bots.rb
+++ bots.rb
@@ -255,7 +255,7 @@ module PuddiServer
     # Create a new thread for this
     Thread.start do
       # Create a server
-      server = TCPServer.new @config.port
+      server = TCPServer.new 5000
       loop do
         # Wait for a request and start a new thread to deal with it.
         Thread.start server.accept do |io|
@@ -290,7 +290,6 @@ class DbooksBot < Ebooks::Bot
     @config.danbooru_key = ENV['DANBOORU_KEY'].chomp
     @config.danbooru_tags = ENV['DANBOORU_TAGS'].chomp
     @config.tweet_interval = ENV['TWEET_INTERVAL'].chomp
-    @config.port = ENV['PORT'].chomp
 
     # Load configuration into twitter variables
     @consumer_key = config.twitter_key
